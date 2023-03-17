2b00432299c9ce2fa183fa359a921f60aa7bd1c0
san <sansamaa0@gmail.com>
2023-03-18 02:53:02 +0300

    hw

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleSubmit(e.target[`new-task-description`].value);
+    form.reset();
+  });
+
+  const deleteButton = document.querySelector("#delete-tasks");
+  deleteButton.addEventListener("click", deleteAllTasks);
 });
+
+function handleSubmit(value) {
+  if (value !== "") {
+    const p = document.createElement(`p`);
+    const btn = document.createElement("button");
+    btn.addEventListener("click", removeTask);
+    btn.innerHTML = `X`;
+    p.textContent = value + " ";
+    p.appendChild(btn);
+    document.querySelector(`#tasks`).appendChild(p);
+  }
+}
+
+function removeTask(e) {
+  e.target.parentNode.remove();
+}
+
+function deleteAllTasks() {
+  document.querySelector(`#tasks`).innerHTML = "";
+}
